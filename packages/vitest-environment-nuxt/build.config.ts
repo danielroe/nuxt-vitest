a60bbdf684efe7b60229b9169ec1cf2e2d073d43
Daniel Roe <daniel@roe.dev>
2023-01-21 22:39:48 +0000

    chore: fix ci build

--- packages/vitest-environment-nuxt/build.config.ts
+++ packages/vitest-environment-nuxt/build.config.ts
@@ -2,6 +2,7 @@ import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig({
   externals: [
+    '#imports',
     '#app/entry',
     '#build/root-component.mjs',
     '@nuxt/schema',
