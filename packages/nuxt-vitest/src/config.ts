eb66dd9495a89e23fd5948b4ac1c583ddb65765c
Daniel Roe <daniel@roe.dev>
2023-05-28 17:57:08 -0500

    fix: narrow down vitest inline patterns

--- packages/nuxt-vitest/src/config.ts
+++ packages/nuxt-vitest/src/config.ts
@@ -112,7 +112,7 @@ export async function getVitestConfigFromNuxt(
             ? true
             : [
                 // vite-node defaults
-                /\/(nuxt|nuxt3)\//,
+                /\/node_modules\/(.*\/)?(nuxt|nuxt3)\//,
                 /^#/,
                 'vite',
                 // additional deps
