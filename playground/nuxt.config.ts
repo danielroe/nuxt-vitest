3dce2a55f39a56c94d3b3d7f10d4add990fa7a10
Daniel Roe <daniel@roe.dev>
2023-08-29 14:43:40 +0100

    test: test built package

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -11,19 +11,6 @@ export default defineNuxtConfig({
   imports: {
     injectAtEnd: true,
   },
-  vite: {
-    // TODO: fix bug with stubbing root package
-    resolve: {
-      alias: {
-        'nuxt-vitest/config': '../packages/vitest-environment-nuxt/src/config',
-        'nuxt-vitest/utils': '../packages/vitest-environment-nuxt/src/utils',
-        'vitest-environment-nuxt/utils':
-          '../packages/vitest-environment-nuxt/src/utils',
-        'vitest-environment-nuxt':
-          '../packages/vitest-environment-nuxt/src/index',
-      },
-    },
-  },
   runtimeConfig: {
     public: {
       hello: 'world',
