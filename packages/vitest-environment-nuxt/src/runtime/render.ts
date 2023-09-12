3d0f7994a64bacc01444cd6a070b2c8084b547b9
Julien Huang <julien.huang@outlook.fr>
2023-09-12 17:18:44 +0200

    fix(vitest-environment-nuxt): import `@testing-library/vue` within `renderSuspended` (#331)

--- packages/vitest-environment-nuxt/src/runtime/render.ts
+++ packages/vitest-environment-nuxt/src/runtime/render.ts
@@ -5,10 +5,8 @@ import {
   h,
   nextTick,
 } from 'vue'
-
-import {
-  type RenderOptions as TestingLibraryRenderOptions,
-  render as renderFromTestingLibrary,
+import type {
+   RenderOptions as TestingLibraryRenderOptions,
 } from '@testing-library/vue'
 import { defu } from 'defu'
 import type { RouteLocationRaw } from 'vue-router'
@@ -67,6 +65,8 @@ export async function renderSuspended<T>(
     route = '/',
     ..._options
   } = options || {}
+
+  const { render : renderFromTestingLibrary } = await import('@testing-library/vue')
 
   // @ts-ignore untyped global __unctx__
   const { vueApp } = globalThis.__unctx__.get('nuxt-app').tryUse()
